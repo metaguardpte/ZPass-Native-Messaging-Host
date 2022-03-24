0ad676857008fdc0b50d79b1d6323a69e1d8d90a
xiaochuang_wang <xiaochuang_wang@fzzixun.com>
2022-03-24 16:04:57 +0800

    Rebranding

--- src/ext-native/product.h
+++ src/ext-native/product.h
@@ -4,8 +4,8 @@
 
 namespace Hubstudio
 {
-    inline constexpr LPCSTR ProdRegistryName = "23ad9c71-5cde-593c-a647-91b7cc008f99";
-    inline constexpr LPCSTR ProdName = "HubStudio-INT";
-    inline constexpr LPCSTR ProdExeName = "HubStudio-INT.exe";
+    inline constexpr LPCSTR ProdRegistryName = "12814cd3-eb4f-5ece-a5ed-822e2ad06f6e";
+    inline constexpr LPCSTR ProdName = "ZeroPassword";
+    inline constexpr LPCSTR ProdExeName = "ZeroPassword.exe";
     inline constexpr LPCSTR ProductPipeName = "BES-3702D6BC-E18A-4CC8-A64A-A5E078E6AC77";
 };
