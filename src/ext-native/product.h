ab69bc6cd049eea6c3f3bb911f2f87443c5b394e
andy <di_wu@fzzixun.com>
2022-08-25 09:59:29 +0800

    rebranding

--- src/ext-native/product.h
+++ src/ext-native/product.h
@@ -5,7 +5,7 @@
 namespace Hubstudio
 {
     inline constexpr LPCSTR ProdRegistryName = "91f3088b-8813-5959-ae3e-3b01deab6da2";
-    inline constexpr LPCSTR ProdName = "ZeroPass";
-    inline constexpr LPCSTR ProdExeName = "ZeroPass.exe";
+    inline constexpr LPCSTR ProdName = "ZPass";
+    inline constexpr LPCSTR ProdExeName = "ZPass.exe";
     inline constexpr LPCSTR ProductPipeName = "BES-3702D6BC-E18A-4CC8-A64A-A5E078E6AC77";
 };
